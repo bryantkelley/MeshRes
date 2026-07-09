801639d93c338d1ce7c5d7a5fa4c74816311b4c9
Bryant Kelley <bryant@bryantkelley.com>
2026-07-09 14:40:25 -0700

    Log received messages, rework port setting

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import {
+  ChannelMessage,
   Constants,
   Message,
   NodeJSSerialConnection,
@@ -8,7 +9,16 @@ import * as process from "node:process";
 
 // Check for environment variables
 if (!process.env.SERIAL_PORT) {
-  throw new Error("Missing SERIAL_PORT");
+  console.error("Missing SERIAL_PORT");
+  process.exit(1);
+}
+
+let wsPort;
+if (!process.env.WS_PORT) {
+  console.log("No WS_PORT provided. Using 3017.");
+  wsPort = 3017;
+} else {
+  wsPort = Number.parseInt(process.env.WS_PORT);
 }
 
 // Set allowed private channels. Channels beginning with an octothorpe are included by default.
@@ -18,13 +28,17 @@ const allowedPrivateChannels = ["Public"];
 const connection = new NodeJSSerialConnection(process.env.SERIAL_PORT);
 
 // Create Web Socket
-const wss = new WebSocketServer({ port: Number.parseInt(process.env.WS_PORT) });
+const wss = new WebSocketServer({ port: wsPort });
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
   ws.on("error", (err) => {
     console.error(err);
   });
+});
+
+wss.on('message', (data) => {
+  console.log('received: %s', data);
 });
 
 // Initial Setup
@@ -44,37 +58,44 @@ connection.on(Constants.PushCodes.MsgWaiting, async () => {
     const waitingMessages = await connection.getWaitingMessages();
 
     waitingMessages.forEach((message: Message) => {
-      // Do not show DMs
-      if (message.channelMessage) {
-        // send channel messages
-        const packet: Message & { channelName: string } = {
-          ...message.channelMessage,
-          channelName: channelNames[message.channelMessage.channelIdx],
-        };
-        console.log(packet.channelName);
-        if (
-          packet.channelName.startsWith("#") ||
-          allowedPrivateChannels.includes(packet.channelName)
-        ) {
-          const separatorIndex = packet.text.trim().indexOf(":");
-          const senderName: string = packet.text.slice(0, separatorIndex);
-          const cleanedMessage: string = packet.text.slice(separatorIndex + 2);
-          const formattedDate = new Date(
-            packet.senderTimestamp * 1000,
-          ).toISOString();
+      if (message) {
+        // Do not show DMs
+        if (Object.keys(message).includes("channelMessage")) {
+          const { channelMessage } = message as {
+            channelMessage: ChannelMessage;
+          };
+          // send channel messages
+          const packet: ChannelMessage & { channelName: string } = {
+            ...channelMessage,
+            channelName: channelNames[channelMessage.channelIdx],
+          };
 
-          // character count before each piece, semicolons between pieces
-          // order of parts: channel, time, sender, message
-          //
-          // Example: `6;Public;24;2026-07-06T18:35:00.000Z;6;moss 🦌;6;howdy!`
-          const formattedPacket = `${packet.channelName.length};${packet.channelName};${formattedDate.length};${formattedDate};${senderName.length};${senderName};${cleanedMessage.length};${cleanedMessage}`;
-          console.log(formattedPacket);
+          if (
+            packet.channelName.startsWith("#") ||
+            allowedPrivateChannels.includes(packet.channelName)
+          ) {
+            const separatorIndex = packet.text.trim().indexOf(":");
+            const senderName: string = packet.text.slice(0, separatorIndex);
+            const cleanedMessage: string = packet.text.slice(
+              separatorIndex + 2,
+            );
+            const formattedDate = new Date(
+              packet.senderTimestamp * 1000,
+            ).toISOString();
 
-          wss.clients.forEach(async (client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(formattedPacket);
-            }
-          });
+            // character count before each piece, semicolons between pieces
+            // order of parts: channel, time, sender, message
+            //
+            // Example: `6;Public;24;2026-07-06T18:35:00.000Z;6;moss 🦌;6;howdy!`
+            const formattedPacket = `${packet.channelName.length};${packet.channelName};${formattedDate.length};${formattedDate};${senderName.length};${senderName};${cleanedMessage.length};${cleanedMessage}`;
+            console.log(formattedPacket);
+
+            wss.clients.forEach(async (client) => {
+              if (client.readyState === WebSocket.OPEN) {
+                client.send(formattedPacket);
+              }
+            });
+          }
         }
       }
     });
